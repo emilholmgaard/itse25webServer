7e6408cf3d295b3cc773e01b15e8a45c0dd8bd39
emilholmgaard <emil@holmgaard.io>
2025-09-08 11:36:54 +0200

    Update server.py

--- src/webserver/server.py
+++ src/webserver/server.py
@@ -1,9 +1,9 @@
-import socket
+from socket import *
 from request_handler import handle_request
 
 def start_server(host, port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     server_socket.bind((host, port))
     server_socket.listen(1)
 
